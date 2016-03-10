c92f91d2f25c9480bec3f1cbadbfec88d35c98b4
Francois GUILLEM <francois.guillem@rte-france.com>
2016-03-10 09:23:50 +0100

    Added function "getLinks" Retrieves links connected to a set of nodes

---
+++ antares/R/getLinks.R
@@ -0,0 +1,23 @@
+#' Retrieve links connected to a set of nodes
+#'
+#' This function find the name of the links connected to a set of node.
+#'
+#' @param nodes
+#' vector containing node names.
+#' @param internalOnly
+#' if TRUE, only links that connect two nodes from the set defined by the
+#' previous argument are returned. If not, the function may return links that
+#' connect a node from the set with a node outside the set.
+#'
+#' @return
+#' character vector containing link names.
+#'
+getLinks <- function(nodes, internalOnly=FALSE) {
+  l <- getOption("antares")$linkList
+  lsplit <- tstrsplit(l, " - ")
+
+  if(internalOnly) idx <- lsplit[[1]] %in% nodes & lsplit[[2]] %in% nodes
+  else idx <- lsplit[[1]] %in% nodes | lsplit[[2]] %in% nodes
+
+  l[idx]
+}
