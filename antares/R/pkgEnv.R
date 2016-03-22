ef722ffa04d23de4fc8495c33319db4c5c2b7654
Francois GUILLEM <francois.guillem@rte-france.com>
2016-03-22 10:00:26 +0100

    Imports data.table instead of depends on it. This solve strange scope problems when testing the package.

--- antares/R/pkgEnv.R
+++ antares/R/pkgEnv.R
@@ -1,10 +1,15 @@
+#' @import data.table
+
 # Private variables accessible only by functions from the package
 
 pkgEnv <- new.env()
 
+# list of column names that are id variables in output files. When they are
+# present in an output file, they are automatically imported, whatever the value
+# of "select" is.
 pkgEnv$idVars <- c("node", "link", "timeId", "day", "week", "month", "hour", "mcYear")
 
-
+# Aliases used by the argument "select" of function readOutput.
 pkgEnv$varAliases <- list(
   economy = c("OV. COST", "OP. COST", "MRG. PRICE", "CO2 EMIS.", "BALANCE", "SPIL. ENRG"),
   adequacy = c("UNSP. ENRG", "LOLD", "LOLP", "AVL DTG", "DTG MRG", "MAX MRG"),
