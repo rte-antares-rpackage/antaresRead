3c69506c05defb86ea09a1754f3140a92e58a3de
françois GUILLEM <francois.guillem@rte-france.com>
2016-04-07 09:39:09 +0200

    extractDataList now adds a Dtime variable containing timestamp of the observation.

---
+++ R/timeIdToDate.R
@@ -0,0 +1,63 @@
+#' format timeId in a readable format
+#' 
+#' This function converts a \code{timeId} variable in a readable format according
+#' to time step. 
+#' 
+#' @return 
+#' A vector with same length than \code{timeId}. The vector class is determined
+#' by timeStep:
+#' For hourly time step, the function returns a timestamp 
+#' (\code{POSIXct}), for daily time step a date and for weekly and monthly 
+#' timestep a character string containing the year and the number of the week or 
+#' the month.
+#' 
+#' @noRd
+#'   
+.timeIdToDate <- function(timeId, timeStep=c("hourly", "daily", "weekly", "monthly", "annual"), 
+                          opts=getOption("antares")) {
+  timeStep <- match.arg(timeStep)
+  
+  if (timeStep == "hourly") {
+    
+    timestamp <- as.POSIXct(opts$start)
+    lubridate::hour(timestamp) <- lubridate::hour(timestamp) + 1:(24*7*52) - 1
+    
+    return(timestamp[timeId])
+    
+  } else if (timeStep == "daily") {
+    
+    date <- as.Date(opts$start)
+    date <- date + 1:(7*52) - 1
+    
+    return(date[timeId])
+    
+  } else if (timeStep == "weekly") {
+    
+    timestamp <- as.POSIXct(opts$start)
+    lubridate::hour(timestamp) <- lubridate::hour(timestamp) + 1:(24*7*52) - 1
+    
+    weekId <- .getTimeId(1:(24*7*52), "weekly", opts)
+    date <- as.Date(tapply(timestamp, weekId, function(x) as.Date(min(x))), origin = "1970-1-1")
+    date[1] <- date[2] - 7
+    
+    week <- format(date, format = "%G-w%V")
+    
+    return(week[timeId])
+    
+  } else if (timeStep == "monthly") {
+    
+    timestamp <- as.POSIXct(opts$start)
+    lubridate::hour(timestamp) <- lubridate::hour(timestamp) + 1:(24*7*52) - 1
+    
+    monthId <- .getTimeId(1:(24*7*52), "monthly", opts)
+    month <- tapply(timestamp, monthId, function(x) format(min(x), format = "%Y-%m"))
+    
+    return(month[timeId])
+
+  } else {
+    
+    year <- lubridate::year(as.POSIXct(opts$start))
+    return(rep(year, length(timeId)))
+    
+  }
+}
