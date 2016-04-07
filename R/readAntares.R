1ba9d214b7c543d7d489f2720e6a387c24cc4501
unknown <jalazawa@GROVSW57.gr0vsdma.rte-france.com>
2016-04-07 19:10:48 +0200

    Change help for showProgress

--- R/readAntares.R
+++ R/readAntares.R
@@ -54,8 +54,8 @@
 #'   data.table is returned. If FALSE, the result will always be a list of class
 #'   "antaresOutput".
 #' @param showProgress
-#'   (logical) shoudl the function display information about the progress of the
-#'   importation ?
+#'   If TRUE show the function display information about the progress of the
+#'   importation.
 #'
 #'
 #' @details If all arguments are unspecified, the default behavior of the
