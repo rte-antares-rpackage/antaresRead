49eedd453f418a2fbb6971aa9ecfaac399e353c5
françois GUILLEM <francois.guillem@rte-france.com>
2016-04-12 15:33:45 +0200

    true nodes are detected using links + costs and CO2 are also corrected

--- R/removeVirtualNodes.R
+++ R/removeVirtualNodes.R
@@ -24,7 +24,6 @@
 #' spec <- list(
 #'   list(
 #'     type = "pumped storage",
-#'     trueNodes = "B",
 #'     virtualNodes = c("psp in", "psp out")
 #'   )
 #' )
@@ -46,24 +45,51 @@ removeVirtualNodes <- function(x, spec) {
   vnodes <- c() # list of virtual nodes that need to be removed at the end
   
   linkList <- unique(x$links$link)
+  nodeList <- unique(x$nodes$node)
   
   for (s in spec) {
     # Make correction and complete the list of virtual nodes
     
     if (s$type == "pumped storage") {
       
-      if (!is.null(x$nodes$BALANCE)) {
+      for (vn in s$virtualNodes) {
         
-        for (tn in s$trueNodes) {
-          for (vn in s$virtualNodes) {
-            if ((l <- paste(tn, vn, sep=" - ")) %in% linkList) {
-              x$nodes[J(tn)]$BALANCE <- x$nodes[J(tn)]$BALANCE - x$links[J(l)]$`FLOW LIN.` 
-            } else if ((l <- paste(vn, tn, sep=" - ")) %in% linkList) {
-              x$nodes[J(tn)]$BALANCE <- x$nodes[J(tn)]$BALANCE + x$links[J(l)]$`FLOW LIN.`
-            }
+        # get all nodes linked to the virtual node
+        links <- ldply(getLinks(vn), function(x) {
+          xx <- strsplit(x, " - ")[[1]]
+          if (xx[1] == vn) return (data.table(link = x, to = xx[2], direction = "out"))
+          else return (data.table(link = x, to = xx[1], direction = "in"))
+        })
+        
+        # Total flow of the virtual node. Used when a single virtual node is
+        # connected to many real nodes in order to distribute costs between them
+        totalFlow <- x$links[J(links$link), .(totalFlow = sum(abs(`FLOW LIN.`))), keyby = timeId]
+        
+        totalFlow <- totalFlow[totalFlow == 0, totalFlow := 1] # To avoid divide by 0 errors
+        
+        # Loop over real nodes connected to the virtual node
+        for (i in 1:nrow(links)) {
+          if (! links$to[i] %in% nodeList) next
+          
+          tn <- links$to[i]
+          l <- links$link[i]
+          dir <- ifelse(links$direction[i] == "in", -1, 1)
+          
+          flow <- dir * x$links[J(l)]$`FLOW LIN.`
+          
+          # Correct balance
+          if (! is.null(x$nodes$BALANCE)) 
+            x$nodes[J(tn)]$BALANCE <- x$nodes[J(tn)]$BALANCE + flow
+          
+          # Correct costs and CO2
+          prop <- abs(flow / totalFlow$totalFlow)
+          
+          for (v in c("OV. COST", "OP. COST", "CO2 EMIS.", "NP COST")) {
+            if (! is.null(x$nodes[[v]]))
+              x$nodes[J(tn)][[v]] <- x$nodes[J(tn)][[v]] + prop * x$nodes[J(vn)][[v]]
           }
+          
         }
-        
       }
       
       vnodes <- union(vnodes, c(s$virtualNodes))
