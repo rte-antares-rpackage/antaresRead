4d130dcf23422f1f7d2c55cf3d92574c02771b98
françois GUILLEM <francois.guillem@rte-france.com>
2016-06-15 14:18:40 +0200

    getLinks gains a new parameter "areas"

--- R/getLinks.R
+++ R/getLinks.R
@@ -1,12 +1,17 @@
 #' Retrieve links connected to a set of areas
 #'
-#' This function find the name of the links connected to a set of areas.
+#' This function find the name of the links connected to a set of areas. 
 #'
+#' @param ... 
+#'   parameters passed to \code{\link{getAreas}} to construct a set of nodes.
+#'   These parameters are used only if argument \code{areas} is \code{NULL}.
 #' @param internalOnly
-#' if TRUE, only links that connect two areas from the list are returned. 
-#' If not, the function may return links that connect a area from the list with 
-#' a area outside the list.
-#' @inheritParams getAreas
+#'   If TRUE, only links that connect two areas from the list are returned. 
+#'   If not, the function may return links that connect a area from the list with 
+#'   a area outside the list.
+#' @param areas
+#'   Vector containing area names. It represents the set of areas we interested
+#'   in. If \code{NULL}, \code{\link{getAreas}} to create the set of areas.
 #' @inheritParams readAntares
 #'
 #' @return
@@ -14,14 +19,11 @@
 #'
 #' @export
 #'
-getLinks <- function(select = NULL, exclude = NULL, regexpSelect = TRUE, 
-                     regexpExclude = TRUE, internalOnly=FALSE, 
-                     opts = simOptions(), ignore.case = TRUE) {
+getLinks <- function(..., opts = simOptions(), internalOnly=FALSE, areas = NULL) {
   l <- opts$linkList
   lsplit <- tstrsplit(l, " - ")
 
-  areas <- getAreas(select, exclude, regexpSelect = regexpSelect, 
-                    regexpExclude = regexpExclude, opts=opts)
+  if(is.null(areas)) areas <- getAreas(..., opts = opts)
   
   if(internalOnly) idx <- lsplit[[1]] %in% areas & lsplit[[2]] %in% areas
   else idx <- lsplit[[1]] %in% areas | lsplit[[2]] %in% areas
