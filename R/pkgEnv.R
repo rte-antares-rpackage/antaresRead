bed1e712c1c5fca0ccb7a457bb0a2fc4b369cad9
françois GUILLEM <francois.guillem@rte-france.com>
2016-07-04 10:36:21 +0200

    Add MISC. NDG and PSP to the list of production variables

--- R/pkgEnv.R
+++ R/pkgEnv.R
@@ -5,31 +5,6 @@
 
 pkgEnv <- new.env()
 
-# list of column names that are id variables in output files. When they are
-# present in an output file, they are automatically imported, whatever the value
-# of "select" is.
-pkgEnv$idVars <- c("area", "district", "sector", "cluster", "link", "mcYear", "timeId", "time", "day", "week", "month", "hour")
-
-# Aliases used by the argument "select" of function readOutput.
-pkgEnv$varAliases <- list(
-  economy = c("OV. COST", "OP. COST", "MRG. PRICE", "CO2 EMIS.", "BALANCE", "SPIL. ENRG"),
-  adequacy = c("UNSP. ENRG", "LOLD", "LOLP", "AVL DTG", "DTG MRG", "MAX MRG"),
-  "net load" = c("ROW BAL.", "PSP", "MISC. NDG", "LOAD", "H. ROR", "WIND", "SOLAR"),
-  generation = c("NUCLEAR", "LIGNITE", "COAL", "GAS", "OIL", "MIX. FUEL", "MISC. DTG", "H. STOR"),
-  ren = c("WIND", "SOLAR", "H. ROR", "H. STOR"),
-  thermal = c("NUCLEAR", "LIGNITE", "COAL", "GAS", "OIL", "MIX. FUEL", "MISC. DTG"),
-  production = c("NUCLEAR", "LIGNITE", "COAL", "GAS", "OIL", "MIX. FUEL",
-                "MISC. DTG", "H. STOR", "H. ROR", "WIND", "SOLAR"),
-  nostat = c("OV. COST", "OP. COST", 
-            "MRG. PRICE", "CO2 EMIS.", "BALANCE", "ROW BAL.", "PSP", "MISC. NDG", 
-            "LOAD", "H. ROR", "WIND", "SOLAR", "NUCLEAR", "LIGNITE", "COAL", 
-            "GAS", "OIL", "MIX. FUEL", "MISC. DTG", "H. STOR", "UNSP. ENRG", 
-            "SPIL. ENRG", "LOLD", "LOLP", "AVL DTG", "DTG MRG", "MAX MRG", 
-            "NP COST", "NODU", "FLOW LIN.", "UCAP LIN.", 
-            "FLOW QUAD.", "CONG. FEE (ALG.)", "CONG. FEE (ABS.)", "MARG. COST", 
-            "CONG. PROB +", "CONG. PROB -", "HURDLE COST")
-)
-
 # Column names in the misc input files.
 pkgEnv$miscNames <- c("CHP", "Bio_mass", "Bio_gas", "Waste", "GeoThermal", "Other", "PSP_input", "ROW_Balance")
 
@@ -43,7 +18,31 @@ pkgEnv$thermal <- c("NUCLEAR", "LIGNITE", "COAL", "GAS", "OIL", "MIX. FUEL", "MI
 pkgEnv$generation <- c("NUCLEAR", "LIGNITE", "COAL", "GAS", "OIL", "MIX. FUEL", "MISC. DTG", "H. STOR")
 
 pkgEnv$production <- c("NUCLEAR", "LIGNITE", "COAL", "GAS", "OIL", "MIX. FUEL",
-              "MISC. DTG", "H. STOR", "H. ROR", "WIND", "SOLAR")
+                       "MISC. DTG", "H. STOR", "H. ROR", "WIND", "SOLAR", "MISC. NDG", "PSP")
+
+# list of column names that are id variables in output files. When they are
+# present in an output file, they are automatically imported, whatever the value
+# of "select" is.
+pkgEnv$idVars <- c("area", "district", "sector", "cluster", "link", "mcYear", "timeId", "time", "day", "week", "month", "hour")
+
+# Aliases used by the argument "select" of function readOutput.
+pkgEnv$varAliases <- list(
+  economy = c("OV. COST", "OP. COST", "MRG. PRICE", "CO2 EMIS.", "BALANCE", "SPIL. ENRG"),
+  adequacy = c("UNSP. ENRG", "LOLD", "LOLP", "AVL DTG", "DTG MRG", "MAX MRG"),
+  "net load" = c("ROW BAL.", "PSP", "MISC. NDG", "LOAD", "H. ROR", "WIND", "SOLAR"),
+  generation = pkgEnv$generation,
+  ren = pkgEnv$ren,
+  thermal = pkgEnv$thermal,
+  production = pkgEnv$production,
+  nostat = c("OV. COST", "OP. COST", 
+            "MRG. PRICE", "CO2 EMIS.", "BALANCE", "ROW BAL.", "PSP", "MISC. NDG", 
+            "LOAD", "H. ROR", "WIND", "SOLAR", "NUCLEAR", "LIGNITE", "COAL", 
+            "GAS", "OIL", "MIX. FUEL", "MISC. DTG", "H. STOR", "UNSP. ENRG", 
+            "SPIL. ENRG", "LOLD", "LOLP", "AVL DTG", "DTG MRG", "MAX MRG", 
+            "NP COST", "NODU", "FLOW LIN.", "UCAP LIN.", 
+            "FLOW QUAD.", "CONG. FEE (ALG.)", "CONG. FEE (ABS.)", "MARG. COST", 
+            "CONG. PROB +", "CONG. PROB -", "HURDLE COST")
+)
 
 # The goal of the following lines is only to remove many useless warnings in 
 # R CMD CHECK: "no visible binding for global variable 'XXX'".
