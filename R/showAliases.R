5928e7cd7922163af280ce012ee263dec56da246
cuche27 <guillem_francois@yahoo.fr>
2017-03-16 16:18:34 +0100

    Doc and tests for addAlias and showAliases

--- R/showAliases.R
+++ R/showAliases.R
@@ -1,19 +1,13 @@
 #Copyright © 2016 RTE Réseau de transport d’électricité
 
 #' show aliases for variables
-#'
-#' This function shows aliases for groups of variables.These aliases can be used
-#' in the \code{\link{readAntares}} function in the \code{select} argument
 #' 
-#' @return
-#' A named list. The names of the list correspond to the aliases and each element
-#' is a vector of variable names.
-#' 
-#' @examples 
-#' showAliases()
+#' @param names optional vector of alias names. If provided, the full list of 
+#'   columns selected by these aliases is displayed. Else only the name and a 
+#'   short description of all aliases is displayed.
 #' 
 #' @export
-#'
+#' @rdname addAlias
 showAliases <- function(names = NULL) {
   desc <- vapply(pkgEnv$varAliases, function(x) x$desc, character(1))
   select <- vapply(pkgEnv$varAliases, FUN.VALUE = character(1), function(x) {
@@ -37,10 +31,50 @@ showAliases <- function(names = NULL) {
   invisible(res)
 }
 
-
+#' Add an "alias" for readAntares
+#' 
+#' @description 
+#' Aliases are short names that can be used in the \code{select} parameter in 
+#' function \code{\link{readAntares}} to tell the function which columns and/or
+#' type of data to import. 
+#' 
+#' \code{addAlias} can be used to create a new alias. It can be especially
+#' useful for package developers to help their users select the data required
+#' by their packages.
+#' 
+#' \code{showAliases} lists available aliases
+#' 
+#' @param name Alias name
+#' @param desc Short description indicating why the new alias is interesting
+#' @param select character vector containing columns and/or types of data to 
+#'   import.
+#' 
+#' @return
+#' \code{addAlias} is only used for its side effects. A data.frame with columns 
+#' 'name', 'desc' and 'select'. \code{showAliases} invisibly returns a 
+#' data.frame with columns "name", "desc" and "select".
+#' 
+#' 
+#' @examples 
+#' 
+#' # Display the short description of an alias
+#' showAliases()
+#' 
+#' # Display the full description of an alias
+#' showAliases("renewable")
+#' 
+#' \dontrun{
+#' # Create a new alias that imports flows
+#' addAlias("test", "short description", c("links", "FLOW LIN.")) 
+#' showAliases()
+#' }
+#' 
+#' @export
 addAlias <- function(name, desc, select) {
   if (!exists("varAliases", envir = pkgEnv)) {
     assign("pkgEnv", list(), envir = pkgEnv)
   }
   pkgEnv$varAliases[[name]] <- list(desc = desc, select = select)
+  
+  invisible(TRUE)
 }
---
+++ tests/testthat/test-test-aliases.R
@@ -0,0 +1,43 @@
+context("test-aliases")
+
+describe("addAlias", {
+  it("adds a new alias", {
+    addAlias("test", "test desc", c("area", "LOAD"))
+    expect_false(is.null(pkgEnv$varAliases$test))
+    expect_equal(pkgEnv$varAliases$test, list(desc = "test desc", select = c("area", "LOAD")))
+  })
+  
+  it ("initializes alias list", {
+    # Remove list of aliases
+    oldList <- pkgEnv$varAliases
+    rm(list = "varAliases", envir = pkgEnv)
+    
+    addAlias("test", "test desc", c("area", "LOAD"))
+    expect_false(is.null(pkgEnv$varAliases))
+    expect_equal(length(pkgEnv$varAliases), 1)
+    expect_false(is.null(pkgEnv$varAliases$test))
+    expect_equal(pkgEnv$varAliases$test, list(desc = "test desc", select = c("area", "LOAD")))
+    
+    pkgEnv$varAliases <- oldList
+  })
+})
+
+describe("showAliases", {
+  it("shows a short description of all aliases", {
+    expect_output(showAliases(), "renewable")
+  })
+  
+  it("returns a data.frame", {
+    expect_output(aliases <- showAliases(), "renewable")
+    expect_is(aliases, "data.frame")
+    expect_equal(nrow(aliases), length(pkgEnv$varAliases))
+    expect_equal(names(aliases), c("name", "desc", "select"))
+  })
+  
+  it("shows the full description of an alias", {
+    expect_output(aliases <- showAliases("renewable"), "SOLAR")
+    expect_is(aliases, "data.frame")
+    expect_equal(nrow(aliases), 1)
+    expect_equal(names(aliases), c("name", "desc", "select"))
+  })
+})
