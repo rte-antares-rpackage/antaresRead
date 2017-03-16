ab6127de164d14b117e34202d26f043b63fa4b9b
cuche27 <guillem_francois@yahoo.fr>
2017-03-16 17:22:05 +0100

    Improve test coverage

---
+++ tests/testthat/test-print.R
@@ -0,0 +1,39 @@
+context("test-print")
+
+opts <- setSimulationPath(studyPath)
+
+describe("print.antaresDataTable", {
+  data <- readAntares(timeStep="annual", showProgress = FALSE)
+  it ("prints basic information", {
+    expected_message <- sprintf("'antaresDataTable' object with dimension %s x %s",
+                                nrow(data), ncol(data))
+    expect_output(print(data), expected_message)
+    expect_output(print(data), "Type: areas")
+    expect_output(print(data), "Synthesis: TRUE")
+  })
+})
+
+describe("print.antaresDataList", {
+  data <- readAntares("all", "all", timeStep="annual", showProgress = FALSE)
+  
+  it("prints basic information", {
+    expect_output(print(data), "'antaresDataList' object with elements areas and links")
+  })
+  
+  it ("prints dimension of each element", {
+    for (e in c("areas", "links")) {
+      expected_message <- sprintf(".$%s (%s x %s)", e, nrow(data[[e]]), ncol(data[[e]]))
+    }
+  })
+  
+  it ("handles case when there is only one element", {
+    data$links <-  NULL
+    expect_output(print(data), "'antaresDataList' object with element areas")
+  })
+})
+
+describe("print.simOptions", {
+  it("prints basic information about the study", {
+    expect_output(print(opts), "Simulation 'test'")
+  })
+})
---
+++ tests/testthat/test-viewAntares.R
@@ -0,0 +1,32 @@
+context("viewAntares")
+
+opts <- setSimulationPath(studyPath)
+
+describe("viewAntares", {
+  with_mock(
+    `utils::View` = function(x, title) {print(title)},
+    {
+      it("views an antaresDataTable", {
+        data <- readAntares(timeStep = "annual", select = "LOAD", showProgress = FALSE)
+        expect_output(viewAntares(data), "data")
+      })
+      
+      it("views an antaresDataList", {
+        data <- readAntares("all", "all", timeStep = "annual", 
+                            select = c("LOAD", "FLOW LIN."), showProgress = FALSE)
+        expect_output(viewAntares(data), "data\\$areas")
+        expect_output(viewAntares(data), "data\\$links")
+      })
+    }
+  )
+  
+  with_mock(
+    `utils::View` = function(x, title) {return(NULL)},
+    {
+      it("displays a warning if there is more than 100 columns", {
+        data <- readAntares(timeStep = "annual", showProgress = FALSE)
+        expect_warning(viewAntares(data), "100 columns")
+      })
+    }
+  )
+})
