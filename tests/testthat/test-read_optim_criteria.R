0d040db1c2c2bafb0b1be97a1f5826a15bda1a41
françois GUILLEM <francois.guillem@rte-france.com>
2017-08-18 16:29:18 +0200

    Add a basic test for readOptimCriteria

---
+++ tests/testthat/test-read_optim_criteria.R
@@ -0,0 +1,13 @@
+context("readOptimCriteria")
+
+opts <- setSimulationPath(studyPath, 1)
+
+describe("readOptimCriteria", {
+  it("returns an antaresDataTable", {
+    optimCrit <- readOptimCriteria(opts)
+    expect_is(optimCrit, "antaresDataTable")
+    expect_equal(attr(optimCrit, "type"), "optimCriteria")
+    expect_equal(attr(optimCrit, "synthesis"), FALSE)
+    expect_equal(attr(optimCrit, "timeStep"), "daily")
+  })
+})
