7b2c1f6fb034a33235c2fe70fdffeb655ccf33fd
Benoit Thieurmel <bthieurmel@gmail.com>
2017-10-25 09:54:35 +0200

    fix typo + format zzz

--- R/zzz.R
+++ R/zzz.R
@@ -99,32 +99,28 @@ rhdf5_message <- "This function require 'rhdf5' (>= 2.20.0) package.
 
 pkgEnvAntareasH5 <- new.env()
 
-
 pkgEnvAntareasH5$varAreas <- c("OV. COST", "OP. COST", "MRG. PRICE", "CO2 EMIS.", "BALANCE",
                                "ROW BAL.", "PSP", "MISC. NDG",  "LOAD", "H. ROR", "WIND", "SOLAR",
                                "NUCLEAR", "LIGNITE", "COAL", "GAS", "OIL", "MIX. FUEL", "MISC. DTG", "H. STOR",
                                "UNSP. ENRG", "SPIL. ENRG", "LOLD", "LOLP", "AVL DTG", "DTG MRG", "MAX MRG", "NP COST", "NODU")
+
 pkgEnvAntareasH5$varAreas <- as.vector(sapply(pkgEnvAntareasH5$varAreas, function(X){paste0(X, c("", "_min", "_max", "_std"))}))
+
 pkgEnvAntareasH5$varDistricts <- pkgEnvAntareasH5$varAreas
 
 pkgEnvAntareasH5$varLinks <- c("FLOW LIN.", "UCAP LIN.", "FLOW QUAD.",
                                "CONG. FEE (ALG.)", "CONG. FEE (ABS.)",
                                "MARG. COST", "CONG. PROB +", "CONG. PROB -", "HURDLE COST")
+
 pkgEnvAntareasH5$varLinks <- as.vector(sapply(pkgEnvAntareasH5$varLinks, function(X){paste0(X, c("", "_min", "_max", "_std"))}))
-
-
 
 pkgEnvAntareasH5$varClusters <- c("production", "NP Cost", "NODU")
 
-
-
-
-
-pkgEnvAntareasH5$varAliasCraeted <- list()
+pkgEnvAntareasH5$varAliasCreated <- list()
 
 
 #misc
-pkgEnvAntareasH5$varAliasCraeted$misc$areas <- c("CHP",
+pkgEnvAntareasH5$varAliasCreated$misc$areas <- c("CHP",
                                                  "Bio_mass",
                                                  "Bio_gas",
                                                  "mustRunWasteTotal",
@@ -132,7 +128,8 @@ pkgEnvAntareasH5$varAliasCraeted$misc$areas <- c("CHP",
                                                  "Other",
                                                  "PSP_input",
                                                  "ROW_Balance")
-pkgEnvAntareasH5$varAliasCraeted$misc$districts <- c("CHP",
+
+pkgEnvAntareasH5$varAliasCreated$misc$districts <- c("CHP",
                                                      "Bio_mass",
                                                      "Bio_gas",
                                                      "mustRunWasteTotal",
@@ -141,60 +138,59 @@ pkgEnvAntareasH5$varAliasCraeted$misc$districts <- c("CHP",
                                                      "PSP_input",
                                                      "ROW_Balance")
 #thermalAvailabilities
-pkgEnvAntareasH5$varAliasCraeted$thermalAvailabilities$clusters <- c("thermalAvailability",
+pkgEnvAntareasH5$varAliasCreated$thermalAvailabilities$clusters <- c("thermalAvailability",
                                                                      "availableUnits")
 
 
 #hydroStorage
-pkgEnvAntareasH5$varAliasCraeted$hydroStorage$areas <- c("hydroStorage")
-pkgEnvAntareasH5$varAliasCraeted$hydroStorage$districts <- c("hydroStorage")
+pkgEnvAntareasH5$varAliasCreated$hydroStorage$areas <- c("hydroStorage")
 
-
+pkgEnvAntareasH5$varAliasCreated$hydroStorage$districts <- c("hydroStorage")
 
 #hydroStorageMaxPower
-pkgEnvAntareasH5$varAliasCraeted$hydroStorageMaxPower$areas <- c("hstorPMaxLow",
+pkgEnvAntareasH5$varAliasCreated$hydroStorageMaxPower$areas <- c("hstorPMaxLow",
                                                                  "hstorPMaxAvg",
                                                                  "hstorPMaxHigh")
 
-pkgEnvAntareasH5$varAliasCraeted$hydroStorageMaxPower$districts <- c("hstorPMaxLow",
+pkgEnvAntareasH5$varAliasCreated$hydroStorageMaxPower$districts <- c("hstorPMaxLow",
                                                                      "hstorPMaxAvg",
                                                                      "hstorPMaxHigh")
 
 #reserve
-pkgEnvAntareasH5$varAliasCraeted$reserve$areas <- c("primaryRes",
+pkgEnvAntareasH5$varAliasCreated$reserve$areas <- c("primaryRes",
                                                     "strategicRes",
                                                     "DSM",
                                                     "dayAhead")
 
-pkgEnvAntareasH5$varAliasCraeted$reserve$districts <- c("primaryRes",
+pkgEnvAntareasH5$varAliasCreated$reserve$districts <- c("primaryRes",
                                                         "strategicRes",
                                                         "DSM",
                                                         "dayAhead")
 
 #linkCapacity
-pkgEnvAntareasH5$varAliasCraeted$linkCapacity$links <- c("transCapacityDirect",
+pkgEnvAntareasH5$varAliasCreated$linkCapacity$links <- c("transCapacityDirect",
                                                          "transCapacityIndirect",
                                                          "impedances",
                                                          "hurdlesCostDirect",
                                                          "hurdlesCostIndirect")
 
 #mustRun
-pkgEnvAntareasH5$varAliasCraeted$mustRun$areas <- c("thermalPmin",
+pkgEnvAntareasH5$varAliasCreated$mustRun$areas <- c("thermalPmin",
                                                     "mustRun",
                                                     "mustRunPartial",
                                                     "mustRunTotal")
 
-pkgEnvAntareasH5$varAliasCraeted$mustRun$districts <- c("thermalPmin",
+pkgEnvAntareasH5$varAliasCreated$mustRun$districts <- c("thermalPmin",
                                                         "mustRun",
                                                         "mustRunPartial",
                                                         "mustRunTotal")
 
-pkgEnvAntareasH5$varAliasCraeted$mustRun$clusters <- c("thermalPmin",
+pkgEnvAntareasH5$varAliasCreated$mustRun$clusters <- c("thermalPmin",
                                                        "mustRun",
                                                        "mustRunPartial",
                                                        "mustRunTotal")
 
-pkgEnvAntareasH5$varAliasCraeted$thermalModulation$clusters <- c("marginalCostModulation",
+pkgEnvAntareasH5$varAliasCreated$thermalModulation$clusters <- c("marginalCostModulation",
                                                                  "marketBidModulation",
                                                                  "capacityModulation",
                                                                  "minGenModulation")
