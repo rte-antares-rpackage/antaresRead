19c4ea35fba4e856c003e2e3f5f40f5ccb63bba2
Benoit Thieurmel <bthieurmel@gmail.com>
2017-11-06 16:23:54 +0100

    succeed with parallel test ?

--- tests/testthat/test-h5_write.R
+++ tests/testthat/test-h5_write.R
@@ -1,14 +1,26 @@
 context("h5 : write data")
 
+# for use travis in parallel
+Sys.unsetenv("R_TESTS")
+
 if(requireNamespace("rhdf5")){
   tpDir2 <- gsub("[\\]", "/", tpDir)
   tptpDir <- paste0(tpDir2, "/tpDir")
-  dir.create(tptpDir)
   
+  dir.create(tptpDir)
   test_that("h5 : write more than one studies mono thread", {
     writeAntaresH5(path = tptpDir,
                    timeSteps = "annual", writeAllSimulations = TRUE, nbCores = 1, 
                    opts = optsG)
+    
+  })
+  unlink(tptpDir, recursive = TRUE)
+  
+  dir.create(tptpDir)
+  test_that("h5 : write more than one studies multi-thread", {
+    writeAntaresH5(path = tptpDir,
+                   timeSteps = "annual", writeAllSimulations = TRUE, 
+                   nbCores = 2, opts = optsG)
     
   })
   unlink(tptpDir, recursive = TRUE)
