d0e21360c19635b9e5c29f0c865a3c67654ab561
Benoit Thieurmel <benoit.thieurmel@datastorm.fr>
2018-06-18 15:04:55 +0200

    readLayout : prevent bug reading multiple studies

--- R/readLayout.R
+++ R/readLayout.R
@@ -66,7 +66,7 @@ readLayout <- function(opts = simOptions(), xyCompare = c("union","intersect")) 
           for(ar in names(init_layout)){
             init_layout[[ar]] <- rbindlist(
               list(init_layout[[ar]][-nrow(init_layout[[ar]])],
-                   tmp_layout[[ar]], init_layout[[ar]][nrow(init_layout[[ar]])])
+                   tmp_layout[[ar]], init_layout[[ar]][nrow(init_layout[[ar]])]), fill = TRUE
             )
             init_layout[[ar]] <- unique(init_layout[[ar]], by = colnames(init_layout[[ar]])[1])
           }
