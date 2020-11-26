c7102a3b33ea721bea4a6d6b5979ec7817d18021
Titouan Robert <titouan.robert@datastorm.fr>
2020-11-26 14:23:12 +0100

    demo 30/09/2020

---
+++ inst/API/demo.R
@@ -0,0 +1,39 @@
+library(jsonlite)
+library(httr)
+library(antaresRead)
+
+###DEMO antares read
+
+##PATH api
+path = "http://localhost:8080/studies/antaresStd/"
+opts <- .setSimulationPathAPI(path, 1)
+
+##PATH file system
+opts2 <- .setSimulationPathAPI("C:/Users/TitouanRobert/Desktop/antaresStd", 1)
+
+
+##Lire des données
+dt1 <-  readAntares(opts = opts)
+dt2 <-  readAntares(opts = opts2)
+
+data.table::fsetequal(dt1, dt2) 
+
+## Les layout
+ly2 <- readLayout(opts = opts)
+ly1 <- readLayout(opts = opts2)
+
+data.table::fsetequal(ly2$areas, ly1$areas)
+data.table::fsetequal(ly2$districts, ly1$districts)
+data.table::fsetequal(ly2$links, ly1$links)
+data.table::fsetequal(ly2$districtLinks, ly1$districtLinks)
+
+## Cluster desc
+cl1 <- readClusterDesc(opts)
+cl2 <- readClusterDesc(opts2)
+data.table::fsetequal(cl1, cl2[, .SD, .SDcols = names(cl1)])
+
+
+##Les inputs
+load1 <- readInputTS(load = 'at', opts = opts)
+load2 <- readInputTS(load = 'at', opts = opts2)
+data.table::fsetequal(load1, load2)
