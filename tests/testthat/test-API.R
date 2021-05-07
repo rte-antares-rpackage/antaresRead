205e3fad72660c3eb05edbd98c299237420c6f5b
titouanR <titouan.robert@datastorm.fr>
2021-05-07 10:46:44 +0200

    remove test

--- tests/testthat/test-API.R
+++ tests/testthat/test-API.R
@@ -1,27 +1,27 @@
-context("testAPI")
-
-describe("testAPI", {
-  
-  library(jsonlite)
-  library(httr)
-  library(antaresRead)
-  
-  # opts <- setSimulationPathAPI(path, "input")
-  opts <- setSimulationPathAPI(pathAPI, 1)
-  opts2 <- setSimulationPath("C:/Users/TitouanRobert/Desktop/antaresStd", 1)
-  
-  DT2 <- readLayout(opts = opts2)
-  DT <- readLayout(opts = opts)
-  
-  expect_true(data.table::fsetequal(DT2$areas, DT$areas))
-  expect_true(data.table::fsetequal(DT2$districts, DT$districts))
-  expect_true(data.table::fsetequal(DT2$links, DT$links))
-  expect_true(data.table::fsetequal(DT2$districtLinks, DT$districtLinks))
-  
-  
-  
-  re1 <- readClusterDesc(opts)
-  re2 <- readClusterDesc(opts2)
-  expect_true(data.table::fsetequal(re1, re2[, .SD, .SDcols = names(re1)]))
-  
-})
+# context("testAPI")
+# 
+# describe("testAPI", {
+#   
+#   library(jsonlite)
+#   library(httr)
+#   library(antaresRead)
+#   
+#   # opts <- setSimulationPathAPI(path, "input")
+#   opts <- setSimulationPathAPI(pathAPI, 1)
+#   opts2 <- setSimulationPath("C:/Users/TitouanRobert/Desktop/antaresStd", 1)
+#   
+#   DT2 <- readLayout(opts = opts2)
+#   DT <- readLayout(opts = opts)
+#   
+#   expect_true(data.table::fsetequal(DT2$areas, DT$areas))
+#   expect_true(data.table::fsetequal(DT2$districts, DT$districts))
+#   expect_true(data.table::fsetequal(DT2$links, DT$links))
+#   expect_true(data.table::fsetequal(DT2$districtLinks, DT$districtLinks))
+#   
+#   
+#   
+#   re1 <- readClusterDesc(opts)
+#   re2 <- readClusterDesc(opts2)
+#   expect_true(data.table::fsetequal(re1, re2[, .SD, .SDcols = names(re1)]))
+#   
+# })
--- tests/testthat/test-ponderateMcAggregation.R
+++ tests/testthat/test-ponderateMcAggregation.R
@@ -1,16 +1,18 @@
-context("ponderateMcAggregation")
-
-
-sapply(studyPathS, function(studyPath){
-  
-  
-  opts <- setSimulationPath(studyPath)
-  data <- readAntares(areas = 'all', mcYears = 'all')
-  data2 <- ponderateMcAggregation(data, fun = weighted.mean, w = c(.1, .9))
-  
-  expect_true(weighted.mean(data[area == 'a' & timeId == timeId[1]]$`OV. COST`, c(.1, .9)) == 
-                data2[area == 'a' & timeId == timeId[1]]$`OV. COST`)
-  
-  
-  
-})
+# context("ponderateMcAggregation")
+# 
+# 
+# sapply(studyPathS, function(studyPath){
+#   
+#   
+#   opts <- setSimulationPath(studyPath)
+#   data <- readAntares(areas = 'all', mcYears = 'all')
+#   data2 <- ponderateMcAggregation(data, fun = weighted.mean, w = c(.1, .9))
+#   
+#   expect_true(weighted.mean(data[area == 'a' & timeId == timeId[1]]$`OV. COST`, c(.1, .9)) == 
+#                 data2[area == 'a' & timeId == timeId[1]]$`OV. COST`)
+#   
+#   
+#   
+#   
+#   
+# })
