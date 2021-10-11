36271acfa996d5677521c9d68ca0595b146870c7
Benoit Thieurmel <benoit.thieurmel@datastorm.fr>
2021-10-11 17:54:01 +0200

    udpate test api enr

--- inst/API/demo_api_enr.R
+++ inst/API/demo_api_enr.R
@@ -4,19 +4,20 @@ library(antaresRead)
 
 # ENR
 host <- "http://pyre.datastorm.fr:8080"
-study_id <- "3eea0b59-0005-451c-8382-39c85fb3e116"
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJpZFwiOiAzLCBcImltcGVyc29uYXRvclwiOiAxLCBcInR5cGVcIjogXCJib3RzXCIsIFwiZ3JvdXBzXCI6IFtdfSIsImlhdCI6MTYzMTE4Mzg3MywibmJmIjoxNjMxMTgzODczLCJqdGkiOiIwZDQwOGE2My1mMWQ1LTRjN2QtOTYwMC1jZmNiOWIzZjFhODMiLCJleHAiOjc5OTAyMjM4NzMsInR5cGUiOiJhY2Nlc3MiLCJmcmVzaCI6ZmFsc2V9.0NqQBmjFEpNAcZAeqV9PXzJipMR15pcsefyAARstwFM"
+study_id <- "178c8e7b-46fa-439e-b44d-0e821787781f"
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJpZFwiOiAyLCBcImltcGVyc29uYXRvclwiOiAxLCBcInR5cGVcIjogXCJib3RzXCIsIFwiZ3JvdXBzXCI6IFtdfSIsImlhdCI6MTYzMzAxMjkzMywibmJmIjoxNjMzMDEyOTMzLCJqdGkiOiJiMDliZThlNS0zZmYwLTRjZjctOWYzYS1kY2NhMzAwYTViYTQiLCJleHAiOjc5OTIwNTI5MzMsInR5cGUiOiJhY2Nlc3MiLCJmcmVzaCI6ZmFsc2V9.NxMxnD_BYDh_vF2aMA17XN6VljE2Glegs31qvH_7_-A"
 simulation = 1
 
 opts_api <- setSimulationPathAPI(host = host, 
                                  study_id = study_id, 
                                  token = token, 
-                                 simulation = 1)
+                                 simulation = 2)
 
 opts_api$parameters$`other preferences`$`renewable-generation-modelling`
+opts_api$parameters$`variables selection`
 
-opts_local <- setSimulationPath("C:\\Users\\BenoitThieurmel\\Desktop\\Antares\\ENR_Cluster\\renewables-2", 1)
-
+opts_local <- setSimulationPath("C:\\Users\\BenoitThieurmel\\Desktop\\Antares\\ENR_Cluster\\test_study_no_output/", 2)
+opts_local$parameters$`variables selection`
 all.equal(opts_api$parameters, opts_local$parameters)
 
 # layout
@@ -77,21 +78,21 @@ for(ts in c("hourly", "daily", "weekly", "monthly", "annual")){
 
 
 dt_local <-  readAntares(opts = opts_local, links = "all", areas = "all", 
-                         clustersRes = "all", districts = "all",
+                         clustersRes = "all", districts = "all", clusters = "all",
                        misc = TRUE, linkCapacity = TRUE, 
                        hydroStorageMaxPower = TRUE,
-                       hydroStorage = TRUE,
+                       hydroStorage = FALSE,
                        thermalModulation = TRUE,
-                       thermalAvailabilities = TRUE,
+                       thermalAvailabilities = FALSE,
                        reserve = TRUE, mustRun = TRUE)
 
 dt_api <-  readAntares(opts = opts_api, links = "all", areas = "all", 
-                       clustersRes = "all", districts = "all",
+                       clustersRes = "all", districts = "all", clusters = "all",
                        misc = TRUE, linkCapacity = TRUE, 
                        hydroStorageMaxPower = TRUE,
-                       hydroStorage = TRUE,
+                       hydroStorage = FALSE,
                        thermalModulation = TRUE,
-                       thermalAvailabilities = TRUE,
+                       thermalAvailabilities = FALSE,
                        reserve = TRUE, mustRun = TRUE)
 
 
@@ -107,18 +108,41 @@ data.table::fsetequal(load_local, load_api)
 
 for(ts in c("hourly", "daily", "weekly", "monthly", "annual")){
   print(ts)
-  fullinput_local <- readInputTS(resProduction = "all",
+  fullinput_local <- readInputTS(load = 'all',
+                                 ror = "all", 
+                                 thermalAvailabilities = "all",
+                                 hydroStorage = "all",
+                                 hydroStorageMaxPower = "all",
+                                 wind = "all",
+                                 solar = "all",
+                                 misc = "all",
+                                 reserve = "all",
+                                 linkCapacity = "all",
                                  opts = opts_local, 
                                  timeStep = ts, showProgress = FALSE)
   
-  fullinput_api <- readInputTS(resProduction = "all",
+  fullinput_api <- readInputTS(load = 'all',  
+                               ror = "all", 
+                               thermalAvailabilities = "all",
+                               hydroStorage = "all",
+                               hydroStorageMaxPower = "all",
+                               wind = "all",
+                               solar = "all",
+                               misc = "all",
+                               reserve = "all",
+                               linkCapacity = "all",
                                opts = opts_api, 
                                timeStep = ts, showProgress = FALSE)
   
   
   print(sapply(1:length(fullinput_local), function(x){
-    data.table::fsetequal(fullinput_local[[x]], fullinput_api[[x]]) 
+    if(!is.null(fullinput_local[[x]]) && nrow(fullinput_local[[x]]) > 0){
+      data.table::fsetequal(fullinput_local[[x]], fullinput_api[[x]]) 
+    } else {
+      TRUE
+    }
   }))
+  
 }
 
 # et en mode "input" ?
@@ -129,7 +153,7 @@ opts_api_input <- setSimulationPathAPI(
   simulation = "input"
 )
 
-opts_local_input <- setSimulationPath(path = "C:\\Users\\BenoitThieurmel\\Desktop\\Antares\\Test_packages_R", "input")
+opts_local_input <- setSimulationPath(path = "C:\\Users\\BenoitThieurmel\\Desktop\\Antares\\ENR_Cluster\\test_study_no_output/", "input")
 
 for(ts in c("hourly", "daily", "weekly", "monthly", "annual")){
   print(ts)
@@ -161,6 +185,10 @@ for(ts in c("hourly", "daily", "weekly", "monthly", "annual")){
   
   
   print(sapply(1:length(fullinput_local), function(x){
-    data.table::fsetequal(fullinput_local[[x]], fullinput_api[[x]]) 
+    if(!is.null(fullinput_local[[x]]) && nrow(fullinput_local[[x]]) > 0){
+      data.table::fsetequal(fullinput_local[[x]], fullinput_api[[x]]) 
+    } else {
+      TRUE
+    }
   }))
 }
