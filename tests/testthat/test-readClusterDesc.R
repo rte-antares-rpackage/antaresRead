06fdf644d84538da8c990f7670c78b50c44cf484
Mahabd <104979479+Nekmek7@users.noreply.github.com>
2024-04-02 14:44:20 +0200

    Read cluster desc bugfix when cluster exist (#242)
    * fix bug readClusterDesc when no cluster + cover ST and RES case
    
    * add test (init test for thermal + res)

--- tests/testthat/test-readClusterDesc.R
+++ tests/testthat/test-readClusterDesc.R
@@ -1,9 +1,42 @@
 
-## v860 ----
+# v860 ----
 
 path_study_test <- grep(pattern = "86", x = studyPathSV8, value = TRUE)
 opts_study_test <- setSimulationPath(path_study_test, simulation = "input")
 
+#minimal columns
+mandatory_cols <- c("area","cluster")
+
+## Thermal ----
+test_that("test read cluster", {
+  
+  # function setSimulationPath() provide areas names with st-storage clusters
+  areas <- opts_study_test$areasWithClusters
+  
+  # read clusters informations
+  input <- readClusterDesc()
+  
+  # tests
+  testthat::expect_true("data.table" %in% class(input))
+  testthat::expect_true(all(areas %in% unique(readClusterDesc()$area)))
+  testthat::expect_true(all(mandatory_cols %in% colnames(input)))
+  testthat::expect_true(nrow(input) == length(input$cluster))
+  
+})
+
+## Renewables ----
+test_that("test read cluster renewables", {
+  
+  # function setSimulationPath() provide areas names with st-storage clusters
+  areas_res <- opts_study_test$areasWithRESClusters
+  
+  #Study not renewables (need activateRES() from antaresEditObject)
+  expect_error(readClusterResDesc(),
+               regexp = "readClusterDesc is available only on studies with 'renewable-generation-modelling'")
+  
+})
+
+## st-storage ----
 test_that("test read cluster st-storage v860", {
   
   # function setSimulationPath() provide areas names with st-storage clusters
@@ -14,5 +47,8 @@ test_that("test read cluster st-storage v860", {
   
   # tests
   testthat::expect_true("data.table" %in% class(input_st))
-  testthat::expect_true(areas_st %in% unique(readClusterSTDesc()$area))
+  testthat::expect_true(all(areas_st %in% unique(readClusterSTDesc()$area)))
+  testthat::expect_true(all(mandatory_cols %in% colnames(input_st)))
+  testthat::expect_true(nrow(input_st) == length(input_st$cluster))
+  
 })
