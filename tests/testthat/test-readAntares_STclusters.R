e9109a56a6dfc13613559f95cb3360b681ea6347
berthetclement <clement.berthet013@gmail.com>
2024-06-27 17:02:47 +0200

    update test to put inside testhat

--- tests/testthat/test-readAntares_STclusters.R
+++ tests/testthat/test-readAntares_STclusters.R
@@ -1,12 +1,11 @@
 #Copyright © 2016 RTE Réseau de transport d’électricité
 
-context("Function readAntares (ST clusters)")
 
-path_study_test <- grep(pattern = "87", x = studyPathSV8, value = TRUE)
 
-opts <- setSimulationPath(path_study_test,simulation="20240105-0934eco")
 
 test_that("ST clusters importation is ok", {
+  path_study_test <- grep(pattern = "87", x = studyPathSV8, value = TRUE)
+  opts <- setSimulationPath(path_study_test,simulation="20240105-0934eco")
   
   clustersST <- readAntares(clustersST="all",timeStep="annual",opts = opts)$clustersST
   expect_true(all(opts$areasWithSTClusters %in% clustersST$area))
