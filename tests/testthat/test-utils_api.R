ab8d2a276aa1d89be603be92948b9a55985c4574
kemihak <kamel.kemiha@rte-france.com>
2025-02-28 12:00:08 +0100

    Remove useless spaces

--- tests/testthat/test-utils_api.R
+++ tests/testthat/test-utils_api.R
@@ -1,12 +1,12 @@
 #Copyright © 2016 RTE Réseau de transport d’électricité
 test_that("import only data related to some areas with 'select'", {
   
-	expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", "", "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
+  expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", "", "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
   expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
   expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", "v1/downloads", ""), "https://antares-web.rte-france.com/api/v1/downloads")
   expect_equal(.generate_endpoint_url("","https://antares-web.rte-france.com/api", "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
   
-	expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", NULL, "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
+  expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", NULL, "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
   expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
   expect_equal(.generate_endpoint_url("https://antares-web.rte-france.com/api", "v1/downloads", NULL), "https://antares-web.rte-france.com/api/v1/downloads")
   expect_equal(.generate_endpoint_url(NULL,"https://antares-web.rte-france.com/api", "v1/downloads"), "https://antares-web.rte-france.com/api/v1/downloads")
