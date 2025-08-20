f2c0eb5922d8c1bdb162537e1371f35baa2bfcf1
berthetclement <clement.berthet013@gmail.com>
2025-08-20 17:53:27 +0200

    work in progress with multiple cluster

--- R/read_st_constraints.R
+++ R/read_st_constraints.R
@@ -5,7 +5,10 @@
 # RULES :
 # read only written files values
 # no default values for TS => return data.table() empty
+#'
+#' @importFrom data.table data.table strsplit rbindlist split
 read_storages_constraints <- function(opts=simOptions()){
+  browser()
   assertthat::assert_that(inherits(opts, "simOptions"))
   stopifnot(opts$antaresVersion>=920)
 
@@ -41,41 +44,100 @@ read_storages_constraints <- function(opts=simOptions()){
 
   df_structured <- cbind(current_files,  df_structured)
 
-  # filter properties
-  df_prop_filtered <- df_structured[grep(pattern = ".ini", x = file)]
+  # re split to have a list by area
+  df_structured_splited <- split(x = df_structured, f = df_structured$area)
 
-  # filter TS values
-  df_ts_filtered <- df_structured[grep(pattern = ".txt", x = file)]
+  # # filter properties
+  # df_prop_filtered <- df_structured[grep(pattern = ".ini", x = file)]
+  #
+  # # filter TS values
+  # df_ts_filtered <- df_structured[grep(pattern = ".txt", x = file)]
+  #
+  # # TODO split/filter by area
+  #
+  # list_prop <- lapply(seq(1, nrow(df_prop_filtered)), function(x){
+  #
+  #   # names structure
+  #   clust_name <- df_prop_filtered[x, cluster_name]
+  #   area_name <- df_prop_filtered[x, area]
+  #
+  #   # read TS
+  #   df_ts <- df_ts_filtered[area %in% area_name,]
+  #   l_ts <- lapply(df_ts$full_path, function(x){
+  #     fread_antares(opts = opts,
+  #                   file = x,
+  #                   integer64 = "numeric",
+  #                   header = FALSE,
+  #                   showProgress = FALSE)
+  #   })
+  #
+  #   names(l_ts) <- sub(pattern = ".txt", replacement = "", x = df_ts$file)
+  #
+  #   # output list
+  #   ll <- list(
+  #     list(properties=readIniFile(df_prop_filtered[x, full_path]),
+  #          values=l_ts))
+  #
+  #   names(ll) <- clust_name
+  #   ll
+  #  })
+  #
+  # names(list_prop) <- df_prop_filtered[, area]
+  # list_prop
 
-  # TODO split/filter by area
+  list_prop <- lapply(names(df_structured_splited), function(x){
 
-  list_prop <- lapply(seq(1, nrow(df_prop_filtered)), function(x){
+    df <- df_structured_splited[[x]]
+
+    # filter properties
+    df_prop_filtered <- df[grep(pattern = ".ini", x = file)]
+
+    # filter TS values
+    df_ts_filtered <- df[grep(pattern = ".txt", x = file)]
 
     # names structure
-    clust_name <- df_prop_filtered[x, cluster_name]
-    area_name <- df_prop_filtered[x, area]
+    cluster_names <- df_prop_filtered[, cluster_name]
+    area_name <- x
 
-    # read TS
-    df_ts <- df_ts_filtered[area %in% area_name,]
-    l_ts <- lapply(df_ts$full_path, function(x){
-      fread_antares(opts = opts,
-                    file = x,
-                    integer64 = "numeric",
-                    header = FALSE,
-                    showProgress = FALSE)
+    # read properties + TS by cluster
+    l_ts <- lapply(cluster_names, function(x_c){
+      # properties part
+      ini_path <- df_prop_filtered[cluster_name %in% x_c, full_path]
+      properties <- readIniFile(ini_path)
+
+      # ts part
+      ts_path <- df_ts_filtered[cluster_name %in% x_c, full_path]
+      ts_file_names <- df_ts_filtered[cluster_name %in% x_c, file]
+
+      if(identical(ts_path, character(0)))
+        return(list(
+          properties=properties,
+          values=data.table()))
+
+      # read one or multiple ts files
+      list_ts <- lapply(ts_path, function(x_ts){
+        ts_value <- fread_antares(opts = opts,
+                                  file = x_ts,
+                                  integer64 = "numeric",
+                                  header = FALSE,
+                                  showProgress = FALSE)
+      })
+
+      names(list_ts) <- sub(pattern = ".txt",
+                            replacement = "",
+                            x = ts_file_names)
+
+      return(list(
+        properties=properties,
+        values=list_ts))
     })
 
-    names(l_ts) <- sub(pattern = ".txt", replacement = "", x = df_ts$file)
+    names(l_ts) <- cluster_names
 
     # output list
-    ll <- list(
-      list(properties=readIniFile(df_prop_filtered[x, full_path]),
-           values=l_ts))
+    l_ts
+  })
 
-    names(ll) <- clust_name
-    ll
-   })
-
-  names(list_prop) <- df_prop_filtered[, area]
+  names(list_prop) <- names(df_structured_splited)
   list_prop
 }
--- tests/testthat/test-read_st_constraints.R
+++ tests/testthat/test-read_st_constraints.R
@@ -1,7 +1,6 @@
 
 
 # v920 ----
-# st-storage ----
 
 # build structure directory
 # input/st-storage/clusters/list.ini
@@ -10,104 +9,284 @@
 # input/st-storage/constraints/{area}/{cluster}/additional-constraints.ini
 # input/st-storage/constraints/{area}/{cluster}/rhs_{constaints}.txt
 
-# mock study
-areas <- c("fr", "be")
-opts <- list(
-  "studyPath" = tempdir(),
-  "inputPath" = file.path(tempdir(), "input"),
-  "typeLoad"= "txt",
-  "areaList" = areas,
-  "antaresVersion" = 920
-)
 
-class(opts) <- c("simOptions")
+test_that("Check opts",{
+  expect_error(
+    read_storages_constraints(opts = list(version=920)),
+    regexp = "opts does not inherit from class simOptions"
+  )
+})
 
-# properties
-fr <- c(
-  "[withdrawal-1]",
-  "variable = withdrawal",
-  "operator = equal",
-  "hours = [1,3,5], [120,121,122,123,124,125,126,127,128]",
-  "",
-  "[withdrawal-2]",
-  "variable = withdrawal",
-  "operator = equal",
-  "hours = [1,3,5], [120,121,122,123,124]"
+test_that("Check minimal version",{
+  # given
+  opts_min <- list(
+    "studyPath" = tempdir(),
+    "inputPath" = file.path(tempdir(), "input"),
+    "typeLoad"= "txt",
+    "areaList" = areas,
+    "antaresVersion" = 910
+  )
+  class(opts_min) <- c("simOptions")
+
+  # then
+  expect_error(
+    read_storages_constraints(opts = opts_min)
+  )
+})
+
+test_that("Check structure of list",{
+
+  # given
+  # mock study
+  areas <- c("fr", "be")
+  opts <- list(
+    "studyPath" = tempdir(),
+    "inputPath" = file.path(tempdir(), "input"),
+    "typeLoad"= "txt",
+    "areaList" = areas,
+    "antaresVersion" = 920
   )
 
-be <- c(
-  "[netting-1]",
-  "variable = netting",
-  "operator = less",
-  "hours = [1, 168]",
-  "",
-  "[netting-2]",
-  "variable = netting",
-  "operator = less",
-  "hours = [1, 167,168,169]"
+  class(opts) <- c("simOptions")
+
+  # properties
+  fr <- c(
+    "[withdrawal-1]",
+    "variable = withdrawal",
+    "operator = equal",
+    "hours = [1,3,5], [120,121,122,123,124,125,126,127,128]",
+    "",
+    "[withdrawal-2]",
+    "variable = withdrawal",
+    "operator = equal",
+    "hours = [1,3,5], [120,121,122,123,124]"
   )
 
-list_properties <- list(fr, be)
+  be <- c(
+    "[netting-1]",
+    "variable = netting",
+    "operator = less",
+    "hours = [1, 168]",
+    "",
+    "[netting-2]",
+    "variable = netting",
+    "operator = less",
+    "hours = [1, 167,168,169]"
+  )
 
-# TS (8760, 1 column)
-TS_VALUE <- matrix(2, 8760)
+  list_properties <- list(fr, be)
 
-# create dir with properties
-cluster_names <- c("storage_1", "storage_2")
-comb <- merge(areas, cluster_names)
+  # TS (8760, 1 column)
+  TS_VALUE <- matrix(2, 8760)
 
-dir_path <- file.path(tempdir(),
-                      "input",
-                      "st-storage",
-                      "constraints",
-                      areas,
-                      paste0(areas, "_", cluster_names))
+  # create dir with properties
+  cluster_names <- c("storage_1", "storage_2")
+  comb <- merge(areas, cluster_names)
 
-# dir_path <- file.path(tempdir(),
-#                       "st-storage",
-#                       "constraints",
-#                       areas,
-#                       paste0(comb$x, "_", comb$y))
+  dir_path <- file.path(tempdir(),
+                        "input",
+                        "st-storage",
+                        "constraints",
+                        areas,
+                        paste0(areas, "_", cluster_names))
 
-lapply(dir_path, dir.create, recursive = TRUE, showWarnings = FALSE)
+  dir_path <- file.path(tempdir(),
+                        "st-storage",
+                        "constraints",
+                        areas,
+                        paste0(comb$x, "_", comb$y))
 
-
-# write properties
-lapply(seq(1, length(list_properties)), function(x){
-  writeLines(list_properties[[x]],
-             file.path(dir_path[x], "additional-constraints.ini"))
-})
-
-# write TS
-names_constraints_1 <- c("withdrawal-1", "withdrawal-2")
-names_constraints_2 <- c("netting-1", "netting-2")
-
-lapply(names_constraints_1, function(x){
-  write.table(TS_VALUE,
-              file = file.path(dir_path[1],
-                               paste0("rhs_",
-                                      x,
-                                      ".txt")),
-              row.names = FALSE,
-              col.names = FALSE)
-})
-
-lapply(names_constraints_2, function(x){
-  write.table(TS_VALUE,
-              file = file.path(dir_path[2],
-                               paste0("rhs_",
-                                      x,
-                                      ".txt")),
-              row.names = FALSE,
-              col.names = FALSE)
-})
+  lapply(dir_path, dir.create, recursive = TRUE, showWarnings = FALSE)
 
 
-test_that("Read without dir 'constraints'",{
+  # write properties
+  lapply(seq(1, length(list_properties)), function(x){
+    writeLines(list_properties[[x]],
+               file.path(dir_path[x], "additional-constraints.ini"))
+  })
+
+  # write TS
+  names_constraints_1 <- c("withdrawal-1", "withdrawal-2")
+  names_constraints_2 <- c("netting-1", "netting-2")
+
+  lapply(names_constraints_1, function(x){
+    write.table(TS_VALUE,
+                file = file.path(dir_path[1],
+                                 paste0("rhs_",
+                                        x,
+                                        ".txt")),
+                row.names = FALSE,
+                col.names = FALSE)
+  })
+
+  lapply(names_constraints_2, function(x){
+    write.table(TS_VALUE,
+                file = file.path(dir_path[2],
+                                 paste0("rhs_",
+                                        x,
+                                        ".txt")),
+                row.names = FALSE,
+                col.names = FALSE)
+  })
 
   # when
   full_st_constraints <- read_storages_constraints(opts = opts)
+
+  # then
+
+  # structure
+  # area
+  expect_equal(names(full_st_constraints), c("be", "fr"))
+
+  # cluster
+  clust_names <- unlist(lapply(full_st_constraints, names),
+                        use.names = FALSE)
+  expect_equal(clust_names, c("be_storage_2", "fr_storage_1"))
+
+  # properties + values
+  struct_names <- unlist(
+    lapply(full_st_constraints, function(x){
+      lapply(x, names)}),
+    use.names = FALSE)
+  expect_equal(unique(struct_names), c("properties", "values" ))
+
+
+  test_that("Check properties",{
+    #given
+    ini_prop <- lapply(seq(1, length(dir_path)), function(x){
+      readIniFile(file = file.path(dir_path[x], "additional-constraints.ini"))
+    })
+
+    names(ini_prop) <- c("fr", "be")
+
+    # then
+    expect_equal(full_st_constraints$fr$fr_storage_1$properties, ini_prop$fr)
+    expect_equal(full_st_constraints$be$be_storage_2$properties, ini_prop$be)
+  })
+
+
+  test_that("Check values",{
+    # given
+
+    # fr
+    ts_fr <- lapply(names_constraints_1, function(x){
+      fread_antares(file = file.path(dir_path[1],
+                                     paste0("rhs_",
+                                            x,
+                                            ".txt")),
+                    opts = opts)
+    })
+
+    # then
+    expect_true(all(
+      file.exists(file.path(dir_path[1],
+                                      paste0("rhs_",
+                                             names_constraints_1,
+                                             ".txt")))))
+
+    expect_equal(ts_fr, list(data.table(TS_VALUE), data.table(TS_VALUE)))
+  })
+
 })
 
 
+test_that("Check with cluster without time series",{
+  # given
+  # mock study
+  areas <- c("fr", "be")
+  opts <- list(
+    "studyPath" = tempdir(),
+    "inputPath" = file.path(tempdir(), "input"),
+    "typeLoad"= "txt",
+    "areaList" = areas,
+    "antaresVersion" = 920
+  )
 
+  class(opts) <- c("simOptions")
+
+  # properties
+  fr <- c(
+    "[withdrawal-1]",
+    "variable = withdrawal",
+    "operator = equal",
+    "hours = [1,3,5], [120,121,122,123,124,125,126,127,128]",
+    "",
+    "[withdrawal-2]",
+    "variable = withdrawal",
+    "operator = equal",
+    "hours = [1,3,5], [120,121,122,123,124]"
+  )
+
+  be <- c(
+    "[netting-1]",
+    "variable = netting",
+    "operator = less",
+    "hours = [1, 168]",
+    "",
+    "[netting-2]",
+    "variable = netting",
+    "operator = less",
+    "hours = [1, 167,168,169]"
+  )
+
+  list_properties <- list(fr, be)
+
+  # TS (8760, 1 column)
+  TS_VALUE <- matrix(2, 8760)
+
+  # create dir with properties
+  cluster_names <- c("storage_3", "storage_4")
+  comb <- merge(areas, cluster_names)
+
+  dir_path <- file.path(tempdir(),
+                        "input",
+                        "st-storage",
+                        "constraints",
+                        areas,
+                        paste0(areas, "_", cluster_names))
+
+  lapply(dir_path, dir.create, recursive = TRUE, showWarnings = FALSE)
+
+
+  # write properties
+  lapply(seq(1, length(list_properties)), function(x){
+    writeLines(list_properties[[x]],
+               file.path(dir_path[x], "additional-constraints.ini"))
+  })
+
+  # write TS
+  names_constraints_1 <- c("withdrawal-1", "withdrawal-2")
+  # names_constraints_2 <- c("netting-1", "netting-2")
+
+  lapply(names_constraints_1, function(x){
+    write.table(TS_VALUE,
+                file = file.path(dir_path[1],
+                                 paste0("rhs_",
+                                        x,
+                                        ".txt")),
+                row.names = FALSE,
+                col.names = FALSE)
+  })
+
+  # when
+  full_st_constraints <- read_storages_constraints(opts = opts)
+
+  # then
+
+  # structure
+  # area
+  expect_equal(names(full_st_constraints), c("be", "fr"))
+
+  # cluster
+  clust_names <- unlist(lapply(full_st_constraints, names),
+                        use.names = FALSE)
+  expect_equal(clust_names, c("be_storage_2", "fr_storage_1"))
+
+  # properties + values
+  struct_names <- unlist(
+    lapply(full_st_constraints, function(x){
+      lapply(x, names)}),
+    use.names = FALSE)
+  expect_equal(unique(struct_names), c("properties", "values" ))
+
+})
